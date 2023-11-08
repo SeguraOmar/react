678b8da0e0faacb6bf8892b7ad6ca2a35c9f2ea2
Omar <omarsegura76300@gmail.com>
2023-11-08 16:48:49 +0100

    .

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './Home/Home.js';
+import { useState } from 'react';
+import App from './About/About';
+function App() {
+
+  const [animation, setAnimation] = useState(true)
+
+  const toggleAnimation = () => {
+    console.log(animation)
+    setAnimation(animation => !animation)
+  }
+
+
+
+  return (
+    <div className="App">
+      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <header className="App-header">
+        <img src={"../images/dwwm_1.png"} className="App-logo" alt="logo" />
+        <p>
+          TP React Développeur Web et Web Mobile
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">Rechercher des films
+        </a>
+        <button className="App-button" onClick={toggleAnimation}>Modifier l’animation</button>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
